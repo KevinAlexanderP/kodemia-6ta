3b20400455edef342bb1d2581b7e2bd03729f439
KevinAlexanderP <kennedyisdead13@gmail.com>
2019-12-03 11:01:11 -0600

    ::Alien:: Dic/1 EjercicioConversiones

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-console.log("Hello Koders!")
+//Dada una variable obtener USD EUROS, CUC?
+/*
+1 USD = 20 MXN
+1 EUR = 21.66 MXN
+1 CUC = 19.54 MXN
+*/
+
+var amountMXN = 190.80
+var USDtoMXN = 19.56
+var CUCtoMXN = 19.54
+var EURtoMXN = 21.66
+var totalMXNToUSD = amountMXN / USDToMXN
+var totalMXNToCUC = amountMXN / CUCtoMXN
+var totalMXNToEUR = amountMXN / EURtoMXN
