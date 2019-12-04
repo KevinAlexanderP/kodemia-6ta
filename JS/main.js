80bc60787941c79723536a806e5e230c612d5e28
KevinAlexanderP <kennedyisdead13@gmail.com>
2019-12-04 11:16:41 -0600

    commit con mas ejercicios :new_moon_with_face:

--- JS/main.js
+++ JS/main.js
@@ -4,6 +4,19 @@ var daysoflife = 2;
 var secondsLiving = "I have lived this seconds " + ((yearsOflife * 365) + (monthsOflife * 30) + daysoflife) * (24 * 60 * 60)
 console.log(secondsLiving);
 
+//Ej palabra perro
+console.log(perros[0] + perros[1] + perros[2] + perros[3] + perros[4]);
+
+//Ej 3
+var number = -300
+if (number >= 0 && number <= 100 && number % 10 === 0) {
+    console.log("entre 0 y 100 y es multiplo de 10")
+} else if (number % 10 === 0) {
+    console.warn("Solo es multiplo de 10")
+} else {
+    console.error('No esta en el rango ni es multiplo perro')
+}
+
 word = "Perros";
 if (word.length % 2 === 0) {
     console.log("la condicion de par se cumple")
