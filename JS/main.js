c29a7178412d92bf02b5dc8b8cd01819908f7d77
KevinAlexanderP <kennedyisdead13@gmail.com>
2019-12-04 11:07:57 -0600

    Class 2 Dic-3 2019 :japanese_ogre:

---
+++ JS/main.js
@@ -0,0 +1,5 @@
+var yearsOflife = 23;
+var monthsOflife = 7
+var daysoflife = 2;
+var secondsLiving = "I have lived this seconds " + ((yearsOflife * 365) + (monthsOflife * 30) + daysoflife) * (24 * 60 * 60)
+console.log(secondsLiving);
