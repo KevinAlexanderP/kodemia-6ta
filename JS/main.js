51313137e580f547a89970da5a3c1d12ea7a882b
KevinAlexanderP <kennedyisdead13@gmail.com>
2019-12-09 21:58:59 -0600

    clase 6

--- JS/main.js
+++ JS/main.js
@@ -1,92 +1,12 @@
-/*
-var numbers = [1,2,3,4,5]
+//metodo
 
-var i=0 ;
+var numbers = [1, 2, 3, 4, 5, 6]
 
-while(i<= numbers.length){
-  console.log(numbers[i]);
-  i++
-
-
-}
-while(i>=0){
-  console.log(numbers[i]);
-  i--
+numbers.map(function(number, index, arr) {
+    return number % 2 > 0
+})
 
 
+var palabras = ["Arnold", "Jose"]
 
-var numbers = [1,2,3,4,5[6,7,8,9,10]]
-
-i=0
-while(numbers > 0 ){
-  console.log(numbers);
-
-
-}
-while(numbers.length)
-{
-  console.log(numbers.length-1)
-}
-
-for( var count=0;count<numbers.length; count ++){
-c
-var palabra = 'woeard'
-var counter = 0
-var vowels = ["a","e","i","o","u"]
-for (var count = 0; count <= palabra.length-1;count++ ){
-   if(vowels.indexOf(palabra[count]) > -1) {
-       console.log(palabra[count])
-       counter++
-   }
-
-}
-if (counter >2 ){
-  console.log("tiene al menos tres vocales")
-}
-
-
-var lista = [ ["David", 4], ["Jose", 2] ]
-var sueldo = 450
-
-
-
-
-for( var i=0, i<lista.length, i++){
-  console.log(lista[i])
-  
-  
-}
-var lista = [ ["David", 4], ["Jose", 2] ]
-var sueldo = 450
-for( i=0; i<lista.length;i++){
-  var hola=i-1
-  console.log(lista[i][i--])
-  
-
-  
-}
-
-}*/
-
-
-var data = [
-    ['a', 'b'],
-    ['c', 'd'],
-    ['e', 'f']
-]
-
-for (var index = 0; index < data.length; index++) {
-    var firstName = data[index][0]
-    var secondName = data[index][1]
-    var fullName = `${firstName}, ${secondName}`
-    console.log(fullName)
-}
-
-var letra1 = palabra[0]
-var letra2 = palabra[1]
-var letra3 = palabra[2]
-var letra4 = palabra[3]
-var letra5 = palabra[4]
-var palabraCompleta = letra1 + letra2 + letra3 + letra4 + letra5
-
-console.log(palabraCompleta)
+palabras.filter(element => element.match(/^a.+/gi))
